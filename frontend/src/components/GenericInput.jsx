46f48ac4766763d4eb5931e76cf180aad604ff0b
Lucas J. <phuttiphongbank@gmail.com>
2025-02-20 05:34:07 +0000

    add id attribute for generic input

--- frontend/src/components/GenericInput.jsx
+++ frontend/src/components/GenericInput.jsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React from 'react';
 
-const GenericInput = ({ label, labelClass='flex text-sm text-left font-medium text-black', type, required=false, onChange, placeholder="", multiple=false, className='block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-customGray-light placeholder:text-customGray-light focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-customOrange-dark sm:text-sm' }) => {
-
-    return (
-        <>
-        <label className={labelClass}>
-            { label }
-        </label>
-        <div className="mt-2">
-            <input
-                type={type}
-                required={required}
-                onChange={onChange}
-                placeholder={placeholder}
-                multiple={multiple}
-                className={className}
-                >
-            </input>
-        </div>
-        </>
-    );
-}
+const GenericInput = ({
+	id,
+	label,
+	labelClass = 'flex text-sm text-left font-medium text-black',
+	type,
+	required = false,
+	onChange,
+	placeholder = '',
+	multiple = false,
+	className = 'block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-customGray-light placeholder:text-customGray-light focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-customOrange-dark sm:text-sm',
+}) => {
+	return (
+		<>
+			<label htmlFor={id} className={labelClass}>
+				{label}
+			</label>
+			<div className="mt-2">
+				<input
+					id={id}
+					type={type}
+					required={required}
+					onChange={onChange}
+					placeholder={placeholder}
+					multiple={multiple}
+					className={className}
+				></input>
+			</div>
+		</>
+	);
+};
 
 export default GenericInput;

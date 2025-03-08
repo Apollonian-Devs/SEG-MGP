6151bc0120821a161cb29cd65722314819410b02
DKatsoulis12 <katsoulisdimitrios04@gmail.com>
2025-03-08 11:35:20 +0000

    Removed support, forget password, and settings; fixed test issues

--- frontend/src/__tests__/TicketDetails.test.jsx
+++ frontend/src/__tests__/TicketDetails.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import TicketDetails from '../components/TicketDetails';
+import { format, parseISO } from 'date-fns';
 
 describe("TicketDetails Component", () => {
     const mockTicket = {
@@ -8,21 +9,20 @@ describe("TicketDetails Component", () => {
       status: "Open",
       priority: "High",
       assigned_to: "John Doe",
-      created_at: "4/2/2024",
+      created_at: "2024-02-04T00:00:00Z", 
       closed_at: null,
     };
-  
+
     test("renders ticket details correctly", () => {
       render(<TicketDetails ticket={mockTicket} />);
-  
+      const formattedDate = format(parseISO(mockTicket.created_at), "dd/MM/yyyy, HH:mm:ss");
+
       expect(screen.getByText(mockTicket.subject)).toBeInTheDocument();
       expect(screen.getByText(`"${mockTicket.description}"`)).toBeInTheDocument();
       expect(screen.getByText(`Status: ${mockTicket.status}`)).toBeInTheDocument();
       expect(screen.getByText(`Priority: ${mockTicket.priority}`)).toBeInTheDocument();
       expect(screen.getByText(`Assigned to: ${mockTicket.assigned_to}`)).toBeInTheDocument();
-      //expect(screen.getByText(`Created at: ${mockTicket.created_at}`)).toBeInTheDocument();
-      expect(screen.getByText(new RegExp(`Created at: ${mockTicket.created_at}`, "i"))).toBeInTheDocument();
-
+      expect(screen.getByText(`Created at: ${formattedDate}`)).toBeInTheDocument();
       expect(screen.getByText("Closed at: Not Set")).toBeInTheDocument();
     });
   

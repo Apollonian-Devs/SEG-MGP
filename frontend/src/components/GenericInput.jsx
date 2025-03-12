a271bb1e8f278153956cd0e8a56cdd8fd80624b6
Lucas J. <phuttiphongbank@gmail.com>
2025-03-12 21:13:01 +0000

    modify generic input to cover radio type

--- frontend/src/components/GenericInput.jsx
+++ frontend/src/components/GenericInput.jsx
@@ -3,31 +3,36 @@ import React from 'react';
 const GenericInput = ({
 	id,
 	label,
-	labelClass = 'flex text-sm text-left font-medium text-black',
+	labelClassName = 'flex text-sm text-left font-medium text-black',
 	type,
 	required = false,
+	checked = false,
 	onChange,
 	placeholder = '',
 	multiple = false,
+	name,
+	value,
+	divClassName = 'flex flex-col gap-2',
 	className = 'block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-customGray-light placeholder:text-customGray-light focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-customOrange-dark sm:text-sm',
 }) => {
 	return (
-		<>
-			<label htmlFor={id} className={labelClass}>
+		<div className={divClassName}>
+			<label htmlFor={id} className={labelClassName}>
 				{label}
 			</label>
-			<div className="mt-2">
-				<input
-					id={id}
-					type={type}
-					required={required}
-					onChange={onChange}
-					placeholder={placeholder}
-					multiple={multiple}
-					className={className}
-				></input>
-			</div>
-		</>
+			<input
+				id={id}
+				type={type}
+				required={required}
+				checked={checked}
+				onChange={onChange}
+				placeholder={placeholder}
+				multiple={multiple}
+				name={name}
+				value={value}
+				className={className}
+			/>
+		</div>
 	);
 };
 

9125a5e26da2a6eb46f54fe9f3651b1103b247c8
Lucas J. <phuttiphongbank@gmail.com>
2025-03-13 05:15:36 +0000

    add sonner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,41 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Register from "./pages/Register";
-import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
+import { Toaster } from 'sonner';
 
 function Logout() {
-  localStorage.clear();
-  return <Navigate to='/' />;
+	localStorage.clear();
+	return <Navigate to="/" />;
 }
 
 function RegisterAndLogout() {
-  localStorage.clear();
-  return <Register />;
+	localStorage.clear();
+	return <Register />;
 }
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route
-          path='/dashboard'
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route path='/register' element={<RegisterAndLogout />} />
-        <Route path='/logout' element={<Logout />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route
+					path="/dashboard"
+					element={
+						<ProtectedRoute>
+							<Toaster richColors position="bottom-center" />
+							<Dashboard />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path="/register" element={<RegisterAndLogout />} />
+				<Route path="/logout" element={<Logout />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;

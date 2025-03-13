33b364c2ac1a09a72d0befc067fe9e519c048b0c
Lucas J. <phuttiphongbank@gmail.com>
2025-03-13 21:43:35 +0000

    add layout

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Layout = ({ children }) => {
+	const location = useLocation();
+	const isHomePage = location.pathname === '/';
+
+	const bodyStyle = {
+		background: 'linear-gradient(to right, #ecbc76 50%, #fffef9 50%)',
+		height: '100vh',
+		margin: '0',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	};
+
+	return (
+		<div
+			style={isHomePage ? { background: '#ecbc76', margin: '0' } : bodyStyle}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default Layout;

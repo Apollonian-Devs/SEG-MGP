6dd2431b8cd52c8bf038957d756873bd2b9e3103
Lucas J. <phuttiphongbank@gmail.com>
2025-03-13 22:00:38 +0000

    add logo to /login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import logo from '../assets/logo.png';
 
 const Login = () => {
 	return (
 		<div className="bg-white w-[40vw] min-w-[300px] max-w-2xl rounded-lg drop-shadow-[0_20px_50px_rgba(0,0,0,0.1)] flex-col justify-start items-center p-10">
 			<div className="flex justify-between p-0 m-0">
-				<p>Welcome to Name</p>
+				<div className="flex items-center justify-center gap-2">
+					<img className="h-10 w-10" src={logo} alt="Logo" />
+					<h3 className="text-medium text-customOrange-dark">
+						Apollonian Devs
+					</h3>
+				</div>
 				<div className="leading-none">
 					<p className="text-xs text-customGray-dark">No Account?</p>
 					<a

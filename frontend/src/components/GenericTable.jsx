8d020df4fb98859e91b3c14ef4b08224393a0319
Lucas J. <phuttiphongbank@gmail.com>
2025-03-15 17:59:13 +0000

    replace select with GenericDropdown

--- frontend/src/components/GenericTable.jsx
+++ frontend/src/components/GenericTable.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import GenericDropdown from './GenericDropdown';
+import GenericButton from './GenericButton';
 
 const GenericTable = ({
 	tableClass = `min-w-full divide-y divide-gray-200`,
@@ -21,26 +23,45 @@ const GenericTable = ({
 
 	const startIndex = (currentPage - 1) * rowsPerPage;
 	const paginatedData = data.slice(startIndex, startIndex + rowsPerPage);
+	const options = [5, 10, 20, 50];
 
-	const handleRowsPerPageChange = (event) => {
-		setRowsPerPage(Number(event.target.value));
+	const handleRowsPerPageChange = (option) => {
+		setRowsPerPage(option);
 		setCurrentPage(1);
 	};
 	return (
 		<>
-			<div className="flex justify-end items-center mb-4">
-				<label className="mr-2 text-gray-700">Rows per page:</label>
-				<select
-					value={rowsPerPage}
-					onChange={handleRowsPerPageChange}
-					className="border-2 border-customOrange-light rounded-md p-1"
-				>
-					<option value={5}>5</option>
-					<option value={10}>10</option>
-					<option value={25}>25</option>
-					<option value={50}>50</option>
-					<option value={100}>100</option>
-				</select>
+			<div className="flex justify-between w-full items-center gap-2 mb-2 text-gray-400">
+				<div className="flex items-center gap-2">
+					<p className=" font-medium">
+						Showing:{' '}
+						<span className="font-normal">
+							{startIndex + 1} - {startIndex + paginatedData.length}
+						</span>{' '}
+						of <span className="font-normal">{data.length}</span>
+					</p>
+				</div>
+				<div className="flex items-center gap-2">
+					<label className="mr-2 font-medium">Rows per page:</label>
+					<GenericDropdown
+						buttonName={`${rowsPerPage} rows`}
+						className="border-2 outline-none border-gray-400 focus:border-customOrange-dark focus:text-customOrange-dark hover:border-customOrange-dark hover:text-customOrange-dark rounded-md transition-colors duration-500 px-2 py-0.5 gap-2"
+						maxHeight="200" // Adjust as needed
+					>
+						<div className="flex flex-col">
+							{options.map((option) => (
+								<GenericButton
+									key={option}
+									value={option}
+									onClick={() => handleRowsPerPageChange(option)}
+									className="w-full text-left hover:bg-gray-300 transition-colors duration-200 px-4 py-2"
+								>
+									{option}
+								</GenericButton>
+							))}
+						</div>
+					</GenericDropdown>
+				</div>
 			</div>
 
 			<div>

31415ce2a56db670331d7a7170c287a658564b21
Lucas J. <phuttiphongbank@gmail.com>
2025-03-20 20:14:33 +0000

    make genericTable 100%

--- frontend/src/__tests__/GenericTable.test.jsx
+++ frontend/src/__tests__/GenericTable.test.jsx
@@ -116,5 +116,46 @@ describe('GenericTable', () => {
     fireEvent.click((screen.getByText('10')));
     expect(screen.getByText('row 0 column 1')).toBeInTheDocument();
   })
+
+
+  it('test redirecting ticket resulting in less total pages', () => {
+    let data = [];
+    for (let i = 0; i < 6; i++) { // Initially 6 entries
+        data.push(
+            {
+                id: i, column1: `row ${i} column 1`, column2: `row ${i} column 2`,
+            }
+        );
+    }
+
+    const { rerender } = render(
+        <GenericTable
+            columnDefinition={columnDefinition}
+            data={data}
+            dataName='test data'
+            rowDefinition={rowDefinition}
+        />
+    );
+
+    expect(screen.getByText('Last')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Last'));
+    expect(screen.getByText('row 5 column 1')).toBeInTheDocument();
+
+    // Now, reduce the data to 5 entries
+    data = data.slice(0, 5);
+
+    rerender(
+        <GenericTable
+            columnDefinition={columnDefinition}
+            data={data}
+            dataName='test data'
+            rowDefinition={rowDefinition}
+        />
+    );
+
+    // Ensure the last row is now row 4, and row 5 is no longer in the document
+    expect(screen.getByText('row 4 column 1')).toBeInTheDocument();
+    expect(screen.queryByText('row 5 column 1')).not.toBeInTheDocument();
+  }); 
 })
   

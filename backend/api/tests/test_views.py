fb0e3f27ed867e988c64eba1d94ba06218831eb3
Adam <adamw0909@gmail.com>
2025-03-22 16:52:05 +0000

    Add full coverage for views

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1378,7 +1378,7 @@ class TestOverdueTicketsView(TestCase):
 
     def create_overdue_ticket(self, test_user):
         
-        created_by, _ = User.objects.get_or_create(username=test_user["username"], defaults=test_user) # suggested by chatGPT
+        created_by, _ = User.objects.get_or_create(username=test_user["username"], defaults=test_user)  
         
         ticket = Ticket.objects.create(
             id=1,
@@ -1413,7 +1413,7 @@ class TestOverdueTicketsView(TestCase):
     
     def create_ticket(self, test_user):
         
-        created_by, _ = User.objects.get_or_create(username=test_user["username"], defaults=test_user) # suggested by chatGPT
+        created_by, _ = User.objects.get_or_create(username=test_user["username"], defaults=test_user) 
         
         ticket = Ticket.objects.create(
             id=1,
@@ -1709,6 +1709,13 @@ class TestDepartmentsListView(TestCase):
         self.assertEqual(response.data[1]["description"], "Handles Housing Issues")
 
 
+    def test_get_request_succeeds_with_no_departments(self):
+        response = self.client.get(reverse("departments-list"))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data, [])
+
     @patch("api.views.Department.objects.all")
     def test_get_request_fails_when_accessing_all_department_objects_raises_an_exception(self, mock_all_departments):
         mock_all_departments.side_effect = Exception("An exception was raised")
@@ -1718,3 +1725,171 @@ class TestDepartmentsListView(TestCase):
         self.assertEqual(response.status_code, 500)
 
 
+#### TEST SUGGESTDEPARTMENTVIEW HERE ####
+class TestSuggestDepartmentView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.student_user = User.objects.create_user(username="testStudent", password="testpass")
+        self.client.force_authenticate(user=self.student_user)
+        self.ticket = Ticket.objects.create(id=1, 
+                                       subject="Test ticket",
+                                       description="This is a test ticket",
+                                       created_by=self.student_user)
+
+
+    def test_post_request_succeeds_with_valid_ticket_id_and_description_with_a_department_in_the_system(self):
+        department = Department.objects.create(name="IT Support", description="Handles IT Issues")
+
+        data = {"ticket_id": self.ticket.id, "description": self.ticket.description}
+
+        response = self.client.post(reverse("suggest-deparment"), data, format="json")
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_post_request_fails_with_status_code_400_if_there_is_invalid_data(self):
+        data = {"ticket_id": self.ticket.id}
+
+        response = self.client.post(reverse("suggest-deparment"), data, format="json")
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["error"], "Ticket ID and description are required.")
+
+
+    def test_post_request_fails_with_status_code_404_if_the_ticket_doesnt_exist(self):
+
+        data = {"ticket_id": 2, "description": self.ticket.description}
+
+        response = self.client.post(reverse("suggest-deparment"), data, format="json")
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data["error"], "Ticket not found.")
+
+
+    def test_post_request_fails_with_status_code_500_if_there_are_no_departments_in_the_system(self):
+        data = {"ticket_id": self.ticket.id, "description": self.ticket.description}
+
+        response = self.client.post(reverse("suggest-deparment"), data, format="json")
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.data["error"], "No departments found in the system.")
+
+    
+    @patch("api.DepartmentSuggestionAI.open")
+    def test_post_request_fails_with_status_code_500_if_the_training_data_file_isnt_found(self, mock_training_data):
+        mock_training_data.side_effect = FileNotFoundError("File not found")
+
+        department = Department.objects.create(name="IT Support", description="Handles IT Issues")
+
+        data = {"ticket_id": self.ticket.id, "description": self.ticket.description}
+
+        response = self.client.post(reverse("suggest-deparment"), data, format="json")
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.data["error"], "Training data file not found.")
+
+
+#### TEST GROUPTICKETSVIEW HERE ####
+class TestGroupTicketsView(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.student_user = User.objects.create_user(username="testStudent", password="testpass")
+
+    
+    def authorize_student(self):
+        test_user = {
+            "username": "@testStudent",
+            "email": "test@email.com",
+            "password": "testpass",
+            "first_name": "first",
+            "last_name": "last"
+        }
+
+        self.client.post(reverse("register"), test_user)
+
+        response = self.client.post(reverse("get_token"), 
+                                    {"username": "@testStudent", 
+                                     "password": "testpass"})
+
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {response.json()["access"]}') 
+
+
+    def authorize_admin(self):
+        test_user = {
+            "username": "@testStaff",
+            "email": "test@email.com",
+            "password": "testpass",
+            "first_name": "first",
+            "last_name": "last",
+            "is_staff": "true",
+            "is_superuser": "true",
+        }
+
+        self.client.post(reverse("register"), test_user)
+
+        response = self.client.post(reverse("get_token"), 
+                                    {"username": "@testStaff", 
+                                     "password": "testpass"})
+        
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {response.json()["access"]}') 
+
+        self.admin, _ = User.objects.get_or_create(username=test_user["username"], defaults=test_user)
+
+
+    def assign_ticket_to_admin(self, ticketID):
+        Ticket.objects.create(
+            id=ticketID,
+            subject=f"Test ticket {ticketID}",
+            description="This is a test ticket",
+            created_by=self.student_user,
+            assigned_to=self.admin
+        )
+
+    
+    def test_get_request_succeeds_with_admin_user_and_more_than_2_tickets_assigned_to_the_admin(self):
+        self.authorize_admin()
+
+        self.assign_ticket_to_admin(1)
+        self.assign_ticket_to_admin(2)
+        self.assign_ticket_to_admin(3)
+
+        response = self.client.get(reverse("user-tickets-grouping"))
+
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_get_request_fails_with_less_than_2_tickets_assigned_to_the_admin(self):
+        self.authorize_admin()
+
+        self.assign_ticket_to_admin(1)
+
+        response = self.client.get(reverse("user-tickets-grouping"))
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["error"], "Not enough tickets available for clustering. Need at least 2.")
+
+    
+    def test_get_request_fails_with_student_user(self):
+        self.authorize_student()
+
+        response = self.client.get(reverse("user-tickets-grouping"))
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data["error"], "Permission denied")
+
+    
+    @patch("api.views.get_tags")
+    def test_get_request_fails_if_get_tags_throws_an_exception(self, mock_get_tags):
+        self.authorize_admin()
+
+        mock_get_tags.side_effect = Exception("An exception was thrown")
+
+        self.assign_ticket_to_admin(1)
+        self.assign_ticket_to_admin(2)
+        self.assign_ticket_to_admin(3)
+
+        response = self.client.get(reverse("user-tickets-grouping"))
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.data["error"], "An error has occurred: An exception was thrown")

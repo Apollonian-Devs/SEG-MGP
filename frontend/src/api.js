d886b27e22aaedc3a4f09429fe12a28d174568c8
Lucas J. <phuttiphongbank@gmail.com>
2025-03-26 17:23:00 +0000

    remove env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/",
 });
 
 export default api;

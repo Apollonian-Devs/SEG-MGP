f062108df8db64054f4a1b64fd0b65245b548de5
Rahat Chowdhury <crahat101@gmail.com>
2025-03-26 15:49:30 +0000

    Need dist folder for deployment

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -47,8 +47,18 @@ DATABASES = {
 
 TEMPLATES = [
     {
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-    }
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # For React's index.html
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 STATICFILES_DIRS = [

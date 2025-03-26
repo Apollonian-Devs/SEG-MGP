5c4c9b89d11134d62202fc6785c979ed88e30876
Rahat Chowdhury <crahat101@gmail.com>
2025-03-26 14:06:34 +0000

    Frontend Deployment Setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    proxy: {
+      '/api': 'https://seg-mgp.onrender.com'
+    },
+  },
+  
   test: {
     globals: true,  
     environment: 'jsdom', 

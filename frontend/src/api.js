c1f21dcf2b835bd687ac01bc8cf823676073405a
Rahat Chowdhury <crahat101@gmail.com>
2025-03-26 20:14:00 +0000

    Change baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import { ACCESS_TOKEN } from "./constants";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api'
+  baseURL: 'apolloniandevs.onrender.com' || '/api'
 });
 
 export default api;

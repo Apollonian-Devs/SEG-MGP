aec99805ae1c7e6b1e20e82030f6164cc83d3194
Rahat Chowdhury <crahat101@gmail.com>
2025-03-27 02:05:22 +0000

    Change scope of backend deployment

--- backend/build.sh
+++ backend/build.sh
@@ -2,20 +2,20 @@
 set -o errexit
 
 # Build frontend
-cd ../frontend
+cd frontend/
 npm install
 npm run build
 
 # Create Django directories
-mkdir -p ../backend/backend/static
-mkdir -p ../backend/backend/templates
+mkdir -p backend/backend/static
+mkdir -p backend/backend/templates
 
 # Copy ALL built files (including assets)
-cp -r dist/* ../backend/backend/static/
-cp dist/index.html ../backend/backend/templates/
+cp -r dist/* backend/backend/static/
+cp dist/index.html backend/backend/templates/
 
 # Return to backend directory
-cd ../backend
+cd /backend/
 
 # Django setup
 python -m pip install --upgrade pip
